5b827ee5a3b8454113929e15e1fe1f5ed9231c8f
alpin54 <alpinn189@gmail.com>
2021-09-02 08:23:18 +0700

    first commit

---
+++ src/scripts/components/index.js
@@ -0,0 +1,12 @@
+/* ------------------------------------------------------------------------------
+@name: Components Index
+@description: Indexing all components
+--------------------------------------------------------------------------------- */
+
+import HeroBanner from "./HeroBanner";
+import WindowResize from "./WindowResize";
+
+export {
+  HeroBanner,
+  WindowResize
+};
