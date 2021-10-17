6e8eba1b2d6e7ff1263100e5c4824986eaa56be5
alpin54 <alpinn189@gmail.com>
2021-10-17 15:49:54 +0700

    update

--- src/scripts/components/Testimonials.js
+++ src/scripts/components/Testimonials.js
@@ -16,29 +16,22 @@ const Testimonials = (() => {
       _selector.owlCarousel('destroy').removeClass('owl-carousel');
     }
 
-    // init carousel more extra small
-    if ($(window).width() > 576) {
-
-      // --- check if itemLength > itemRun
-      if (_itemLength > _itemRun) {
-        // --- init carousel
-        _selector.addClass('owl-carousel').owlCarousel({
-          items: 1,
-          autoWidth: true,
-          dots: true,
-          nav: true,
-          navText: ["<i class='fi-arrow-right'></i>", "<i class='fi-arrow-right'></i>"],
-          touchDrag: true,
-          mouseDrag: false,
-          dotsSpeed: 1000,
-          dragEndSpeed: 800
-        });
-      } else {
-        if (_selector.hasClass('owl-carousel')) {
-          _selector.removeClass('owl-carousel');
-        }
-        _selector.addClass('hero-banner--single');
-      }
+    // --- check if itemLength > itemRun
+    if (_itemLength > _itemRun) {
+      // --- init carousel
+      _selector.addClass('owl-carousel').owlCarousel({
+        items: 1,
+        margin: 24,
+        autoHeight: true,
+        autoWidth: true,
+        dots: true,
+        nav: true,
+        touchDrag: true,
+        mouseDrag: true,
+        dotsSpeed: 1000,
+        navSpeed: 1000,
+        dragEndSpeed: 800
+      });
     }
   }
 
