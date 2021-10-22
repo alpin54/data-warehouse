0a0c0346db29eb57f9a832ab8666d738f43f77fb
alpin54 <alpinn189@gmail.com>
2021-10-22 16:05:03 +0700

    update

--- src/scripts/components/Testimonials.js
+++ src/scripts/components/Testimonials.js
@@ -7,8 +7,8 @@ const Testimonials = (() => {
 
   // handleRunCarousel
   const handleRunCarousel = () => {
-    const _selector =  $('.js-testimonials');
-    const _itemLength = $('.js-testimonials .testimonials__item').length;
+    const _selector =  $('.js-testimonials-list');
+    const _itemLength = $('.js-testimonials-list .testimonials__item').length;
     const _itemRun = 1;
 
     // destroy carousel
@@ -18,20 +18,39 @@ const Testimonials = (() => {
 
     // --- check if itemLength > itemRun
     if (_itemLength > _itemRun) {
+      console.log(1);
       // --- init carousel
       _selector.addClass('owl-carousel').owlCarousel({
         items: 1,
-        margin: 24,
+        rewind: false,
+        autoplay: true,
         autoHeight: true,
         autoWidth: true,
         dots: true,
         nav: true,
+        loop: false,
         touchDrag: true,
         mouseDrag: true,
+        autoplayHoverPause: true,
+        autoplayTimeout: 8000,
         dotsSpeed: 1000,
         navSpeed: 1000,
-        dragEndSpeed: 800
+        autoplaySpeed: 1000,
+        dragEndSpeed: 800,
+        responsive: {
+          0: {
+            margin: 16
+          },
+          992: {
+            margin: 24
+          }
+        }
       });
+    } else {
+      if (_selector.hasClass('owl-carousel')) {
+        _selector.removeClass('owl-carousel');
+      }
+      _selector.addClass('testimonials--single');
     }
   }
 
